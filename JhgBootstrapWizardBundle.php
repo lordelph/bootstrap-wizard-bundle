dfb8b79a9171d79e17f0994eaf9e797d8f3c6741
Javier Hernández Gil <bilbo@deverbena.com>
2013-12-12 00:34:35 +0100

    Resources reorganize and added bundle and composer file

---
+++ JhgBootstrapWizardBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace Jhg\BootstrapWizardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ * @author Javi Hernández Gil <bilbo@deverbena.com>
+ */
+class JhgBootstrapWizardBundle extends Bundle
+{
+	
+}
