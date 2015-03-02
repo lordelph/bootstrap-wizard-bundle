4f47b24d9399b3324789bb7e5afff13681069605
Paul Dixon <paul@liblynx.com>
2015-03-02 08:07:09 +0000

    renamed bundle

---
+++ ElphinBootstrapWizardBundle.php
@@ -0,0 +1,13 @@
+<?php
+namespace Elphin\BootstrapWizardBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ * @author Paul Dixon <paul@elphin.com>
+ */
+class ElphinBootstrapWizardBundle extends Bundle
+{
+
+}
